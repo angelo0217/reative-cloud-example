a4dc76152f83cd163d094adee2386f5cf01fdc76
LAPTOP-36RLR11G\ACER <dean.wu@rhf-tech.com>
2020-08-25 10:43:59 +0800

    add reactive redis and normal redis and redis cache

--- stream-client/src/main/java/com/stream/client/controller/RedisController.java
+++ stream-client/src/main/java/com/stream/client/controller/RedisController.java
@@ -5,7 +5,6 @@ import com.stream.common.model.RedisVo;
 import com.stream.common.model.UserVo;
 import com.stream.common.util.JsonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.ReactiveHashOperations;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,18 +27,6 @@ public class RedisController {
 
     @Autowired
     private CacheService cacheService;
-
-    @GetMapping("/redis1")
-    public void test(){
-        ReactiveHashOperations<String, String, String> hashOps = reactiveRedisTemplate.opsForHash();
-        Mono mono1 = hashOps.put("apple", "x", "6000");
-        mono1.subscribe(System.out::println);
-        Mono mono2 = hashOps.put("apple", "xr", "5000");
-        mono2.subscribe(System.out::println);
-        Mono mono3 = hashOps.put("apple", "xs max", "8000");
-        mono3.subscribe(System.out::println);
-
-    }
 
     @GetMapping("/deleteVal")
     public Flux deleteVal() {
