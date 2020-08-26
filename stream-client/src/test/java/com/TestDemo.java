8f332cecfb855f36d3ae911777ccdf58909da73f
LAPTOP-36RLR11G\ACER <dean.wu@rhf-tech.com>
2020-08-26 12:17:49 +0800

    add test

--- stream-client/src/test/java/com/TestDemo.java
+++ stream-client/src/test/java/com/TestDemo.java
@@ -3,7 +3,10 @@ package com;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TestDemo {
@@ -25,5 +28,34 @@ public class TestDemo {
         for(int i = 0 ; i< 10 ; i++){
             Flux.just(i).subscribe(value -> System.out.println(value+"  "));
         }
+    }
+
+    @Test
+    public void test3(){
+        Flux.combineLatest(
+                Arrays::toString,
+                Flux.interval(Duration.of(0, ChronoUnit.MILLIS),
+                        Duration.of(100, ChronoUnit.MILLIS)).take(2),
+                Flux.interval(Duration.of(50, ChronoUnit.MILLIS),
+                        Duration.of(100, ChronoUnit.MILLIS)).take(2)
+        ).toStream().forEach(System.out::println);
+    }
+
+    @Test
+    public void test4(){
+        Flux.mergeSequential(
+                Flux.just(11,12,13,14,15),
+                Flux.just(1,2,3,4,5))
+                .toStream()
+                .forEach(System.out::println);
+//        System.out.println("---");
+//        Flux.mergeSequential(Flux.interval(
+//                Duration.of(0, ChronoUnit.MILLIS),
+//                Duration.of(100, ChronoUnit.MILLIS)).take(2),
+//                Flux.interval(
+//                        Duration.of(50, ChronoUnit.MILLIS),
+//                        Duration.of(100, ChronoUnit.MILLIS)).take(2))
+//                .toStream()
+//                .forEach(System.out::println);
     }
 }
